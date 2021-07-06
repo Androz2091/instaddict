6f55a2e0ee7aadc33cb72e0958eaf85d09a5ff27
Androz2091 <androz2091@gmail.com>
2021-07-06 10:35:43 +0200

    :pencil: Correct help page

--- src/views/Help.svelte
+++ src/views/Help.svelte
@@ -1,17 +1,5 @@
 <div class="help">
-    <div>
-        Here is a short TikTok video ✨ to show you how to request your Discord Data Package! Note that this is also possible on mobile device using the iOS and Android Discord apps!
-    </div>
-    <div class="tiktok-player">
-        <!-- svelte-ignore a11y-media-has-caption -->
-        <video controls width="250">
-
-            <source src="https://github.com/Androz2091/discord-data-package-explorer/raw/master/assets/help-tiktok.mp4"
-                    type="video/webm">
-        
-            Sorry, your browser doesn't support embedded videos.
-        </video>
-    </div>
+    <p>this page is not available yet</p>
 </div>
 
 <style>
